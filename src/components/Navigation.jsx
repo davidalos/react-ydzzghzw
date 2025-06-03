45505ec4a3db493674c7ac86b7bb9679415dce64
davidalos <davidalexanderostergaard@gmail.com>
2025-06-03 01:35:30 +0000

    Update navigation with correct logo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -24,7 +24,7 @@ export function Navigation() {
               <Link to="/" className="block">
                 <img
                   className="h-12 w-auto"
-                  src="https://images.pexels.com/photos/1148820/pexels-photo-1148820.jpeg?auto=compress&cs=tinysrgb&w=256"
+                  src="/og copy.png"
                   alt="Kópavogsbær"
                 />
               </Link>
